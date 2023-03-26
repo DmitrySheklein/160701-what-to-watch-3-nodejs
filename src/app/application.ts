de48eb9a9daf0b1dce411f15812d48ca5d93815d
Дмитрий <shcheklein.dev@gmail.com>
2023-03-26 17:56:56 +0300

    Fix: test Application

--- src/app/application.ts
+++ src/app/application.ts
@@ -7,6 +7,7 @@ import { DatabaseInterface } from '../common/database-client/database.interface.
 import { getUri } from '../utils/db.js';
 import { CommentServiceInterface } from '../modules/comment/comment-service.interface.js';
 import { FilmServiceInterface } from '../modules/film/film-service.interface.js';
+import { UserServiceInterface } from '../modules/user/user-service.interface.js';
 
 @injectable()
 export default class Application {
@@ -16,6 +17,7 @@ export default class Application {
     @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface,
     @inject(Component.FilmServiceInterface) private filmService: FilmServiceInterface,
     @inject(Component.CommentServiceInterface) private commentService: CommentServiceInterface,
+    @inject(Component.UserServiceInterface) private userService: UserServiceInterface,
   ) {}
 
   public async init() {
@@ -31,17 +33,19 @@ export default class Application {
     );
 
     await this.databaseClient.connect(uri);
+    this.userService.findByEmail('sfsf@sgsgsg.ru');
 
     const comment = await this.commentService.create({
       message: `My comment ${new Date().toISOString()}`,
-      rating: Math.floor(Math.random() * 10),
+      rating: 10,
       postDate: new Date(),
-      userId: '63e8a82d66e29469756b99c7',
-      filmId: '63e8a82e66e29469756b99ca',
+      userId: '64203e5cc388ef9e7d53e35d',
+      filmId: '642050e07a0ba612a72c199b',
     });
     console.log(comment);
-
-    const films = await this.filmService.find();
-    console.log(films);
+    const c = await this.commentService.findByFilmId('64203e5cc388ef9e7d53e35f');
+    console.log(c);
+    const f = await this.filmService.find(2);
+    console.log(f);
   }
 }
