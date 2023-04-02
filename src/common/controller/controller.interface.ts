78f72a1d42aee5e7781236c963ff378a015fb613
Дмитрий <shcheklein.dev@gmail.com>
2023-04-02 17:58:50 +0300

    6.4. Добавит интерфейс ControllerInterface.

---
+++ src/common/controller/controller.interface.ts
@@ -0,0 +1,11 @@
+import { RouteIntreface } from '../../types/route.interface.js';
+import { Response, Router } from 'express';
+
+export interface ControllerINterface {
+  readonly router: Router;
+  addRoute(route: RouteIntreface): void;
+  send<T>(res: Response, statusCode: number, data: T): void;
+  ok<T>(res: Response, data: T): void;
+  created<T>(res: Response, data: T): void;
+  noContent<T>(res: Response, data: T): void;
+}
