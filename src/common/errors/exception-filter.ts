d7b21f76c7a6581e3cc6bdccc44c3bc0cf6daa9f
Dmitry <sheklein.dima@gmail.com>
2023-04-03 21:52:28 +0300

    6.17. Добавит класс ExceptionFilter.

---
+++ src/common/errors/exception-filter.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/component.types';
+import { LoggerInterface } from '../logger/logger.interface';
+import { ExceptionFilterInterface } from './exception-filter.interface';
+import StatusCodes from 'http-status-codes';
+
+@injectable()
+export default class ExceptionFilter implements ExceptionFilterInterface {
+  constructor(@inject(Component.LoggerInterface) private logger: LoggerInterface) {
+    this.logger.info('Register ExceptionFilter');
+  }
+
+  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+    this.logger.error(error.message);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message });
+  }
+}
