e1fcdcc10f83844f3f17765b990c1190815488aa
Dmitry <sheklein.dima@gmail.com>
2023-04-07 20:31:37 +0300

    Fix: UpdateUserDto

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsString, Length } from 'class-validator';
+
 export default class UpdateUserDto {
-  public userName!: string;
-  public avatarUrl!: string;
+  @IsString({ message: 'firstname is required' })
+  @Length(1, 15, { message: 'Min firstname length is 1, max is 15' })
+  public firstname!: string;
+
+  @IsString({ message: 'avatarPath is required' })
+  public avatarPath!: string;
 }
