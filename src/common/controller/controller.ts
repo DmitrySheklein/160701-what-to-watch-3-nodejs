0505c244cbb37302539f14b96f39d63f960a3903
Dmitry <sheklein.dima@gmail.com>
2023-04-07 16:55:00 +0300

    7.13. Добавит регистрацию middleware в Controller.

--- src/common/controller/controller.ts
+++ src/common/controller/controller.ts
@@ -19,7 +19,12 @@ export abstract class Controller implements ControllerInterface {
   }
 
   public addRoute(route: RouteIntreface): void {
-    this._router[route.method](route.path, asyncHandler(route.handler.bind(this)));
+    const routeHandler = asyncHandler(route.handler.bind(this));
+    const middlewares = route.middlewares?.map((middleware) =>
+      asyncHandler(middleware.execute.bind(middleware)),
+    );
+    const allHanlders = middlewares ? [...middlewares, routeHandler] : routeHandler;
+    this._router[route.method](route.path, allHanlders);
     this.logger.info(`Route registered: ${route.method.toUpperCase()} ${route.path}`);
   }
 
