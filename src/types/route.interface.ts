38e2d61790cefe4c046218347579a9bb0dc53563
Dmitry <sheklein.dima@gmail.com>
2023-04-07 16:44:46 +0300

    7.12. Расширит интерфейс RouteInterface.

--- src/types/route.interface.ts
+++ src/types/route.interface.ts
@@ -1,8 +1,10 @@
 import { HttpMethod } from './http-method.enum.js';
 import { NextFunction, Request, Response } from 'express';
+import { MiddleWareInterface } from './middleware.interface.js';
 
 export interface RouteIntreface {
   path: string;
   method: HttpMethod;
   handler: (req: Request, res: Response, next: NextFunction) => void;
+  middlewares?: MiddleWareInterface[];
 }
