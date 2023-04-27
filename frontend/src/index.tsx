fc5ae981acb733c801dc68b749a961214c88636d
Дмитрий <shcheklein.dev@gmail.com>
2023-04-27 10:27:51 +0300

    Fix: удаление лишнего запроса

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,9 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import App from './components/app/app';
 import { store } from './store';
-import { checkAuth, fetchFilms } from './store/api-actions';
+import { checkAuth } from './store/api-actions';
 import 'react-toastify/dist/ReactToastify.css';
 
-store.dispatch(fetchFilms());
 store.dispatch(checkAuth());
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
